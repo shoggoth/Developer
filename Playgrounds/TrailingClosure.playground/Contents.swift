3da21956fece86b1294e5905dd75384f2377eaa7
Rich Henry <rich@icloud.com>
2017-04-20 12:10:59 +0100

    Trailing closure syntax experiments

--- Playgrounds/TrailingClosure.playground/Contents.swift
+++ Playgrounds/TrailingClosure.playground/Contents.swift
@@ -2,4 +2,34 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+class MyFunClass {
+
+    func doSomething(activity: String, handler: (String) -> Void) {
+
+    }
+}
+
+let fun = MyFunClass()
+
+// From https://twitter.com/NatashaTheRobot
+// That is rather old. Probably the syntax has changed since 2014
+// fun.doSomething("fun", handler: { (funString) -> Void in })
+
+class MyClass {
+
+    func doSomeStringMagic(str: String, handler: (String) -> Void) {
+
+        _ = handler(str)
+    }
+
+    func doSomeStringLenMagic(str: String, handler: (String) -> (Int, String)) -> (Int, String) {
+
+        return handler(str)
+    }
+}
+
+let my = MyClass()
+
+// Here it is with and without the trailing closure syntax
+my.doSomeStringMagic(str: "Hello, World!", handler: { foo in print("Not using it \(foo)") })
+my.doSomeStringMagic(str: "Hello, World!") { foo in print("Using it \(foo)") }
