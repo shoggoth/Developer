c0ca5422678110565e66f648f8d02d9d8a9735e1
Rich Henry <rich.henry@easysoft.com>
2017-08-08 14:05:31 +0100

    Another adaptive stack view columnar layout example. Layout with an embedded table view controller.

--- Projects/Experiments/TableView/ColumnarTableViewController.swift
+++ Projects/Experiments/TableView/ColumnarTableViewController.swift
@@ -8,48 +8,48 @@
 
 import UIKit
 
-class ColumnarTableViewController: UITableViewController {
+private let data = [["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4456", "Data5"],
+                    ["Data1", "Data2456", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3456", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5456"],
+                    ["Data1456", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "DataL", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+                    ["Data1", "Data2", "Data3", "Data4", "Data5"],
+]
 
-    private let data = [["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4456", "Data5"],
-                        ["Data1", "Data2456", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3456", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5456"],
-                        ["Data1456", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "DataL", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ["Data1", "Data2", "Data3", "Data4", "Data5"],
-                        ]
+class ColumnarTableViewController: UITableViewController {
 
     override func viewDidLoad() {
 
@@ -63,7 +63,9 @@ class ColumnarTableViewController: UITableViewController {
     }
 
     override func didReceiveMemoryWarning() {
+
         super.didReceiveMemoryWarning()
+
         // Dispose of any resources that can be recreated.
     }
 
@@ -84,67 +86,15 @@ class ColumnarTableViewController: UITableViewController {
 
             for (index, value) in row.enumerated() {
 
-                print("Installed = \(dataCell.dataLabels[index].isInstalled())")
-
                 dataCell.dataLabels[index].text = value
             }
         }
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 class ColumnCell : UITableViewCell {
 
     @IBOutlet var dataLabels: [UILabel]!
-}
-
-private extension UIView {
-
-    func isInstalled() -> Bool { return self.superview != nil }
 }
