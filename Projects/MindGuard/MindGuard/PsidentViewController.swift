04f5e8fbaa22d283714bb4b985fafbf3ad4438c6
Rich Henry <rich@icloud.com>
2017-09-14 14:48:17 +0100

    Checking in before I leave easysoft

--- Projects/MindGuard/MindGuard/PsidentViewController.swift
+++ Projects/MindGuard/MindGuard/PsidentViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
+@IBDesignable class InvaderView : UIView {
+
+    override class var layerClass : AnyClass { return InvaderLayer.self }
+
+    override func draw(_ rect: CGRect) {} // so that layer will draw itself
+}
+
+class InvaderLayer : CALayer {
+
+    @NSManaged var thickness : CGFloat
+
+    override class func needsDisplay(forKey key: String) -> Bool {
+
+        if key == #keyPath(thickness) { return true }
+
+        return super.needsDisplay(forKey:key)
+    }
+
+    override func draw(in con: CGContext) {
+
+        let r = self.bounds.insetBy(dx:0, dy:0)
+
+        con.setFillColor(UIColor.red.cgColor)
+        con.fill(r)
+        con.setStrokeColor(UIColor.blue.cgColor)
+        con.setLineWidth(self.thickness)
+        con.stroke(r)
+    }
+
+    override func action(forKey key: String) -> CAAction? {
+
+        if key == #keyPath(thickness) {
+
+            let ba = CABasicAnimation(keyPath: key)
+            ba.fromValue = self.presentation()?.value(forKey:key)
+
+            return ba
+        }
+
+        return super.action(forKey:key)
+    }
+}
+
 class PsidentViewController: UIViewController {
 
+    @IBOutlet var invaderView: InvaderView!
+
+    // Actions
+
+    @IBAction func borderSwitchValueChanged(_ sender: UISwitch) {
+
+        (invaderView.layer as? InvaderLayer)?.thickness = sender.isOn ? 40 : 0
+    }
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        (invaderView.layer as? InvaderLayer)?.thickness = 0.0
     }
 
     override func didReceiveMemoryWarning() {
