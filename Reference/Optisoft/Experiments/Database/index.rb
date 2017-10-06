6f11bbe4c3a7aa6203d633d5402e0dcaf39a2903
rich@anubis <rich@dogstar.ltd>
2017-10-06 17:39:14 +0100

    Deleted Reference for re-add

--- Reference/Optisoft/Experiments/Database/index.rb
+++
@@ -1,5 +0,0 @@
-require 'sqlite3'
-
-db = SQLite3::Database.new("test.db")
-rows = db.execute("select * from users")
-p rows[0]
--- Reference/Optisoft/Experiments/Foo/Bar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#  Bar.sh
-#  Foo
-#
-#  Created by Optisoft Ltd on 24/09/2013.
-#
