e0b0f3066b92fbab27036366ee1b39cf9564e0de
rich@anubis <rich@dogstar.ltd>
2017-10-18 14:24:35 +0100

    Deleted Optisoft from Reference

--- Reference/Optisoft/OptiLib/OptiLib/DSActor.m
+++
@@ -1,9 +0,0 @@
-//
-//  DSActor.m
-//  Dogstar Engine (unified version)
-//
-//  Created by Richard Henry on 01/01/2011.
-//  Copyright 2011 Dogstar Diversions. http://www.dogstar.mobi
-//
-
-#import "DSActor.h"
--- Reference/Optisoft/OptiLib/OptiLib/DSSimulator/SpringAndMass/dsmsSpring.m
+++
@@ -1,9 +0,0 @@
-//
-//  dsmsSpring.c
-//  Dogstar Engine (unified version)
-//
-//  Created by Richard Henry on 01/01/2011.
-//  Copyright 2011 Dogstar Diversions. http://www.dogstar.mobi
-//
-
-#include "dsmsSpring.h"
