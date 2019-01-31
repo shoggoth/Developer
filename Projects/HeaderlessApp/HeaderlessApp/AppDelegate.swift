1c721ba6dfc3f0f56d0beddc55ca6cb73e2a4c4c
rich@anubis <r.j.henry@gmail.com>
2019-01-31 15:50:15 +0000

    Added inception Headerless app for Smoothwall preparations

---
+++ Projects/HeaderlessApp/HeaderlessApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HeaderlessApp
+//
+//  Created by Richard Henry on 31/01/2019.
+//  Copyright © 2019 Dogstar Industries Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Projects/HeaderlessApp/HeaderlessApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeaderlessApp
+//
+//  Created by Richard Henry on 31/01/2019.
+//  Copyright © 2019 Dogstar Industries Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
