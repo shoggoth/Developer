f33751f9f51a5573ff101163d5acb08a09e2c249
rich@anubis <r.j.henry@gmail.com>
2020-01-08 16:12:25 +0000

    A few more bits added to the revision exercises

--- Playgrounds/Revision 2020.playground/Sources/Classes.swift
+++ Playgrounds/Revision 2020.playground/Sources/Classes.swift
@@ -1,5 +1,24 @@
 import Foundation
 
+class TupleElements {
+    
+    var num: Int = 3
+    
+    var tup: (num: Int, name: String?) {
+        
+        get { return (num, "Bona Fide") }
+        
+        set { num = newValue.num }
+    }
+    
+    deinit { print("Going away") }
+}
+
 public func classExperiments() {
     
+    let elem = TupleElements()
+    
+    elem.tup = (27, nil)
+    
+    print(elem.tup)
 }
