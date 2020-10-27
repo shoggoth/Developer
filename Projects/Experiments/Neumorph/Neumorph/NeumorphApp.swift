465dbbd7f9e96512eed3e86f0dca29edaadc2426
rich@anubis <r.j.henry@gmail.com>
2020-10-27 11:20:32 +0000

    Neumorphism experiments

---
+++ Projects/Experiments/Neumorph/Neumorph/NeumorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphApp.swift
+//  Neumorph
+//
+//  Created by Richard Henry on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
