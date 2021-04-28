6f585546f588470e82f364d9d907a5a73fba24d9
rich@anubis <r.j.henry@gmail.com>
2021-04-28 16:26:07 +0100

    Started on the PointPattern

---
+++ Playgrounds/Pattern.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+enum PointPattern {
+    
+    case rectangle(w: Int, h: Int)
+    case circle(divs: Int)
+    
+    func trace(size: CGFloat, with:(CGPoint) -> Void) {
+        
+        switch self {
+        case let .rectangle(w, h):
+            
+            let off = size * 0.5
+            (0..<h).forEach { y in (0..<w).forEach { x in with(CGPoint(x: CGFloat(x) * size - off, y: CGFloat(y) * size - off)) }}
+            
+            case let .circle(divs):
+                print("Circle with \(divs)")
+        }
+    }
+}
+
+PointPattern.rectangle(w: 2, h: 3).trace(size: 1) { p in print("Rect 1 \(p)") }
+PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p in print("Rect 2 \(p)") }
+PointPattern.rectangle(w: 2, h: 2).trace(size: 4) { p1 in
+    PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p2 in
+        print("Rect 3 \(CGPoint(x: p1.x + p2.x, y: p1.y + p2.y))")
+    }
+}
