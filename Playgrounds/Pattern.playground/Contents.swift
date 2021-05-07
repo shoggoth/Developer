393bb09ef082d76a61a505e940fb419997847a79
rich@basilisk <r.j.henry@gmail.com>
2021-05-07 12:56:30 +0100

    Circle pattern

--- Playgrounds/Pattern.playground/Contents.swift
+++ Playgrounds/Pattern.playground/Contents.swift
@@ -13,16 +13,26 @@ enum PointPattern {
             let off = size * 0.5
             (0..<h).forEach { y in (0..<w).forEach { x in with(CGPoint(x: CGFloat(x) * size - off, y: CGFloat(y) * size - off)) }}
             
-            case let .circle(divs):
-                print("Circle with \(divs)")
+        case let .circle(divs):
+            
+            let inc = (.pi * 2.0) / Double(divs)
+            
+            (0..<divs).forEach { d in
+                
+                let angle = Double(d) * inc
+                with(CGPoint(x: CGFloat(sin(angle)) * size, y: CGFloat(cos(angle)) * size)) }
         }
     }
 }
 
-PointPattern.rectangle(w: 2, h: 3).trace(size: 1) { p in print("Rect 1 \(p)") }
-PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p in print("Rect 2 \(p)") }
-PointPattern.rectangle(w: 2, h: 2).trace(size: 4) { p1 in
-    PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p2 in
-        print("Rect 3 \(CGPoint(x: p1.x + p2.x, y: p1.y + p2.y))")
-    }
-}
+let s = "BumSex"
+let g: (CGPoint) -> Void = { p in print("\(s) \(p)") }
+
+PointPattern.circle(divs: 4).trace(size: 1) { p in g(p) }
+//PointPattern.rectangle(w: 2, h: 3).trace(size: 1) { p in print("Rect 1 \(p)") }
+//PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p in print("Rect 2 \(p)") }
+//PointPattern.rectangle(w: 2, h: 2).trace(size: 4) { p1 in
+//    PointPattern.rectangle(w: 2, h: 2).trace(size: 2) { p2 in
+//        print("Rect 3 \(CGPoint(x: p1.x + p2.x, y: p1.y + p2.y))")
+//    }
+//}
