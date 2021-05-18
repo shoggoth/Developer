3aeb9ba3cb12f3be680c2add5460755e35572869
rich@anubis <r.j.henry@gmail.com>
2021-05-18 09:47:46 +0100

    Circular shape image

--- Projects/Experiments/Starter/ItemRow.swift
+++ Projects/Experiments/Starter/ItemRow.swift
@@ -11,12 +11,16 @@ import SwiftUI
 struct ItemRow: View {
     
     let item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
             }
         }
