fcddac2f31ebd7ecd94bbe5694cdad71938056a7
rich@anubis <r.j.henry@gmail.com>
2021-06-10 11:59:09 +0100

    Unmoving text

--- Projects/JadeTime/JadeTime/RootScene.swift
+++ Projects/JadeTime/JadeTime/RootScene.swift
@@ -12,12 +12,12 @@ class RootScene: SKScene {
     private var feedbackNode = SKSpriteNode()
     private lazy var jadeNode = { self.childNode(withName: "//JadeNode") as! SKSpriteNode }()
     private lazy var rttRoot = { self.childNode(withName: "//RTT_Root")! }()
+    private lazy var hudRoot = { self.childNode(withName: "//HUD_Root")! }()
 
     private var lastUpdateTime = TimeInterval.zero
     
     required init?(coder aDecoder: NSCoder) {
         
-        feedbackNode = SKSpriteNode()
         feedbackNode.size = UIScreen.main.bounds.size
         feedbackNode.color = .red
         feedbackNode.setScale(1.1)
@@ -45,10 +45,12 @@ class RootScene: SKScene {
     
     func renderTexture(to node: SKSpriteNode) {
         
+        hudRoot.isHidden = true
         if let texture = self.view?.texture(from: scene!) {
             
             //rttNode.texture = texture
             feedbackNode.run(.setTexture(texture, resize: true)) // https://www.hackingwithswift.com/example-code/games/how-to-change-a-sprites-texture-using-sktexture
         }
+        hudRoot.isHidden = false
     }
 }
