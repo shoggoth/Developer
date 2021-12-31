9ad4eb01a803b8b05cef0c7d93dcb3fc0a58e307
rich@wight <r.j.henry@gmail.com>
2021-12-31 11:33:50 +0000

    Inception aspect ratio test bed. SK Editor fixed?

---
+++ Projects/Experiments/AspectRatio/DebugComponent.swift
@@ -0,0 +1,40 @@
+//
+//  DebugComponent.swift
+//  GameMechanics
+//
+//  Created by Richard Henry on 01/05/2020.
+//  Copyright © 2020 Dogstar Industries Ltd. All rights reserved.
+//
+
+import GameplayKit
+
+public class DebugComponent: GKComponent {
+    
+    @GKInspectable var identifier: String = UUID().uuidString
+    @GKInspectable var dumpTiming: Bool = false
+    
+    deinit { print(" \(self.identifier) \(self) deinits") }
+    
+    // MARK: Update
+    
+    public override func update(deltaTime: TimeInterval) {
+        
+        super.update(deltaTime: deltaTime)
+        
+        if dumpTiming { print("\(self) update at \(deltaTime)s") }
+    }
+    
+    public override func didAddToEntity() {
+        
+        print("DebugComponent '\(self.identifier)' (\(self)) added to entity: \(String(describing: entity))")
+        //print("Sprite component on add: \(String(describing: entity?.spriteComponent))")
+    }
+    
+    public override func willRemoveFromEntity() {
+        
+        print("Component \(self) will remove from entity \(String(describing: entity))")
+        //print("Sprite component on remove: \(String(describing: entity?.spriteComponent))")
+    }
+    
+    public override class var supportsSecureCoding: Bool { return true }
+}
